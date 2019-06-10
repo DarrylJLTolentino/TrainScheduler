687bce259928508f679b80d9a31624cce423242a
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-09 17:45:58 -0700

    Adding firebase config data and a variable for firebase database function within app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCHJDdfS9tH2QCBJWAh9da6VfeNIyMVhck",
+    authDomain: "testproject-68489.firebaseapp.com",
+    databaseURL: "https://testproject-68489.firebaseio.com",
+    projectId: "testproject-68489",
+    storageBucket: "testproject-68489.appspot.com",
+    messagingSenderId: "317718905956",
+    appId: "1:317718905956:web:fdc72cdcce4456ee"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
